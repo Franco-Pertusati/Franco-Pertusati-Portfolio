05627a689502140858007be4ebd403a5f9d60bbc
Franco Pertusati <pertusatifranco836@gmail.com>
2024-07-22 15:38:32 -0300

    Revert "portfolio"
    This reverts commit bfecf34c16cc77b1d8674486838134b75990cf9b.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PortfolioComponent } from './pages/portfolio/portfolio.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { BackgroundComponent } from './shared/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    PortfolioComponent,
-    NavbarComponent,
-    BackgroundComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Portfolio-Angular';
+  title = 'testBuild2';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { PortfolioComponent } from './pages/portfolio/portfolio.component';
-import { BackgroundComponent } from './shared/background/background.component';
 
-export const routes: Routes = [
-    {
-        path: '',
-        component: PortfolioComponent
-    },
-    {
-        path: 'wasd',
-        component: BackgroundComponent
-    }
-];
+export const routes: Routes = [];
