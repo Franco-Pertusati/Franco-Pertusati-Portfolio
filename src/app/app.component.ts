bfecf34c16cc77b1d8674486838134b75990cf9b
Franco Pertusati <pertusatifranco836@gmail.com>
2024-07-22 15:32:26 -0300

    portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BackgroundComponent } from './shared/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PortfolioComponent,
+    NavbarComponent,
+    BackgroundComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'testBuild2';
+  title = 'Portfolio-Angular';
 }
